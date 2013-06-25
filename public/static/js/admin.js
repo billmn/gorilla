3b5ec799a4207be14b65d52ae9667218d61faf6b
Davide Bellini <davide.bellini@selfcomposer.com>
2013-06-25 17:38:38 +0200

    Implementato TinyMCE

--- public/static/js/admin.js
+++ public/static/js/admin.js
@@ -1,16 +1,54 @@
 $(function()
 {
-	// Foundation scripts
+	/*
+	|--------------------------------------------------------------------------
+	| Foundation scripts
+	|--------------------------------------------------------------------------
+	*/
 	$(document).foundation();
 
-	// Confirm dialog
+	/*
+	|--------------------------------------------------------------------------
+	| Confirm dialog
+	|--------------------------------------------------------------------------
+	*/
 	$(document).on('click', '.confirm', function()
 	{
 		var answer = confirm(confirm_question);
 		if ( ! answer) return false;
 	});
 
-	// Select 2
+	/*
+	|--------------------------------------------------------------------------
+	| Select 2
+	|--------------------------------------------------------------------------
+	*/
 	$('select').select2();
+
+	/*
+	|--------------------------------------------------------------------------
+	| TinyMCE
+	|--------------------------------------------------------------------------
+	*/
+	tinymce.init({
+		selector           : 'textarea.wysi',
+		theme              : 'modern',
+		width              : '100%',
+		menubar            : false,
+		entity_encoding    : false,
+		plugins            : [
+			"advlist anchor autolink autoresize charmap code contextmenu directionality emoticons",
+			"fullpage fullscreen hr image insertdatetime legacyoutput link lists media nonbreaking noneditable pagebreak",
+			"paste preview save searchreplace tabfocus table visualchars wordcount textcolor"
+		],
+
+		toolbar1 : "newdocument undo redo | cut copy paste removeformat | searchreplace | charmap emoticons | table | formatselect fontsizeselect | forecolor backcolor",
+		toolbar2 : "bold italic underline strikethrough | hr | superscript subscript | bullist numlist | outdent indent blockquote | alignleft aligncenter alignright alignjustify | link unlink | image media | code fullscreen",
+
+		setup : function(editor)
+		{
+
+		}
+	});
 
 })
