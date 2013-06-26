4f71fe50578fe8a5024f84528a732480c9a90fb8
Davide Bellini <davide.bellini@selfcomposer.com>
2013-06-26 18:26:51 +0200

    Messaggio generico per login utente disabilitato sulle API

--- app/controllers/api/ApiBaseController.php
+++ app/controllers/api/ApiBaseController.php
@@ -7,7 +7,7 @@ class ApiBaseController extends Controller {
 		if ( ! Auth::user()->enabled)
 		{
 			header('HTTP/1.1 401 Unauthorized', true, 401);
-			exit('Your user id disabled');
+			exit('Authentication Failed');
 		}
 	}
 
